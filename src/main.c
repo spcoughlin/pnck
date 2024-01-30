5402d57fefea0d6bc71948dcc56ae66f64268854
Sean Coughlin <56spc56@gmail.com>
2024-01-30 11:41:44 -0500

    file opening

--- src/main.c
+++ src/main.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <ncurses.h>
+#include "main.h"
 
-int main() {
-    initscr();
-    printw("Hello World!");
-    refresh();
-    getch();
-    endwin();
+void openFile(char *file) {
+	FILE *fp;
+	int c;
+	fp = fopen(file, "r");
+	while ((c = fgetc(fp)) != EOF) {
+		printw("%c", c);
+	}
+	fclose(fp);
+}
 
-    return 0;
+int main(int argc, char *argv[]) {
+        initscr();
+        if (argc == 2) {
+                char *file = argv[1];
+                // open and display file
+		openFile(file);
+        }
+        printw("Hello World!");
+        refresh();
+        getch();
+        endwin();
+
+        return 0;
 }
 
---
+++ src/main.h
@@ -0,0 +1,1 @@
+void openFile(char *fileName);
