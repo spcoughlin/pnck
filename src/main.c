69667bfbf29f909fa59cdb2ef3fbf29a7fdc02cf
Sean Coughlin <56spc56@gmail.com>
2024-02-01 09:48:42 -0500

    change buffer func to use ptr

--- src/main.c
+++ src/main.c
@@ -3,9 +3,7 @@
 #include <ncurses.h>
 #include "main.h"
 
-Buffer open_file_to_buffer(char *file) {
-	Buffer buffer;
-
+Buffer open_file_to_buffer(Buffer* buffer, char *file) {
 	FILE *fp;
 	fp = fopen(file, "r");
 
@@ -19,26 +17,26 @@ Buffer open_file_to_buffer(char *file) {
 	}
 
 	// make buffer rows
-	buffer.num_rows = lines;
-	buffer.rows = malloc(sizeof(Row) * buffer.num_rows);
-	if (!buffer.rows) {
+	buffer->num_rows = lines;
+	buffer->rows = malloc(sizeof(Row) * buffer->num_rows);
+	if (!buffer->rows) {
 		perror("malloc");
 		fclose(fp);
-		return buffer;
+		return *buffer;
 	}
 	
 	// allocate mem for row content
 	for (int i = 0; i < lines; i++) {
-		buffer.rows[i].length = 140;
-		buffer.rows[i].contents = malloc(sizeof(char) * buffer.rows[i].length);
-		if (!buffer.rows[i].contents) {
+		buffer->rows[i].length = 140;
+		buffer->rows[i].contents = malloc(sizeof(char) * buffer->rows[i].length);
+		if (!buffer->rows[i].contents) {
 			perror("malloc");
 			fclose(fp);
 			for (int j = 0; j < i; j++) {
-				free(buffer.rows[j].contents);
+				free(buffer->rows[j].contents);
 			}
-			free(buffer.rows);
-			return buffer;
+			free(buffer->rows);
+			return *buffer;
 		}
 	}
 
@@ -46,36 +44,37 @@ Buffer open_file_to_buffer(char *file) {
 	rewind(fp);
 	int cur_line = 0, char_num = 0;
 	while((ch = fgetc(fp)) != EOF && cur_line < lines) { 
-		if (char_num < buffer.rows[cur_line].length - 1) {
-			buffer.rows[cur_line].contents[char_num++] = ch;
+		if (char_num < buffer->rows[cur_line].length - 1) {
+			buffer->rows[cur_line].contents[char_num++] = ch;
 			if (ch == '\n') {
-				buffer.rows[cur_line].contents[char_num] = '\0';
+				buffer->rows[cur_line].contents[char_num] = '\0';
 				cur_line++;
 				char_num = 0;
 			}
 		}
 	}
 	fclose(fp);
-	return buffer;
+	return *buffer;
 }
 
-void print_buffer_to_screen(Buffer buffer) {
-	for (int i = 0; i < buffer.num_rows; i++) {
-		printw("Buffer Row %i: %s", i, buffer.rows[i].contents);
+void print_buffer_to_screen(Buffer *buffer) {
+	for (int i = 0; i < buffer->num_rows; i++) {
+		printw("Buffer Row %i: %s", i, buffer->rows[i].contents);
 	}
 }
 
 int main(int argc, char *argv[]) {
         initscr();
 	Buffer buffer;
+	Buffer *buffer_ptr = &buffer;
         if (argc == 2) {
                 char *file = argv[1];
                 // open and display file
-		buffer = open_file_to_buffer(file);
+		open_file_to_buffer(buffer_ptr, file);
         } else {
-		buffer = open_file_to_buffer("empty.txt");
+		open_file_to_buffer(buffer_ptr, "empty.txt");
 	}
-	print_buffer_to_screen(buffer);
+	print_buffer_to_screen(buffer_ptr);
         printw("\nWaiting for input to close");
 	refresh();
 	getch();
